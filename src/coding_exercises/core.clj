9f9f18b376b43544add8a73a276bf606b3c764b4
David Petrovics <dpetrovics@gmail.com>
2012-07-26 17:20:49 -0400

    fibonacci sequence

--- src/coding_exercises/core.clj
+++ src/coding_exercises/core.clj
@@ -1,4 +1,20 @@
 (ns coding-exercises.core
   (:require [clojure.string :as str]))
 
+(defn basic-fib
+  ^{:doc "Returns the n-th number in the fibonacci sequence."}
+  [num]
+  (if (= num 0)
+    0
+    (if (= num 1)
+      1
+      (+ (basic-fib (- num 2))
+         (basic-fib (- num 1))))))
+
+(defn lazy-fib
+  ^{:doc "Returns a lazy sequnence of the fibonacci sequence. Use with 'take' or 'nth'"}
+  []
+  (map first
+       (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
+
 
