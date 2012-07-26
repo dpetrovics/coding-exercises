89ace76bad069c6d0ea0320c8c130e13a0296218
David Petrovics <dpetrovics@gmail.com>
2012-07-26 18:40:02 -0400

    clock angle problem

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject coding-exercises "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/math.numeric-tower "0.0.1"]])
--- src/coding_exercises/core.clj
+++ src/coding_exercises/core.clj
@@ -1,5 +1,6 @@
 (ns coding-exercises.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math.numeric-tower :as math]))
 
 (defn basic-fib
   ^{:doc "Returns the n-th number in the fibonacci sequence."}
@@ -16,5 +17,16 @@
   []
   (map first
        (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
+
+(defn clock-angle
+  ^{:doc "Finds the angle in degrees between the hour hand and minute hand."}
+  [hr min]
+  (assert (and (< min 60) (>= min 0)) "invalid minute")
+  (let [min-angle (* min
+                     (/ 360 60))
+        hr-angle (* (+ (mod hr 12) (/ min 60))
+                    (/ 360 12))]
+    (float
+     (math/abs (- hr-angle min-angle)))))
 
 
